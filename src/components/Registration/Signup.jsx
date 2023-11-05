662e32dda3855761f137ffc5672cb661c775f62f
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-11-06 00:55:29 +0100

    the images and icons needed

---
+++ src/components/Registration/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './signup.css';
+
+const Signup = () => {
+  return (
+    <div>
+      <div className="signup"></div>
+    </div>
+  )
+}
+
+export default Signup;
