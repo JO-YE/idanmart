4b4158c8f1eccc2da453d239fc81162f94f83f4a
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-11-14 19:50:10 +0100

    linking page

--- src/components/Registration/Register.js
+++ src/components/Registration/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./Login_nav";
 import Signup from "./Signup";
 import Signin from "./Signin";
@@ -7,13 +7,11 @@ import Signin from "./Signin";
 const Register = () => {
   return (
     <div>
-      <Router>
-        <Login /> {/* this will appear in all the registration component*/}
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/sign-in" element={<Signin />} />
-        </Routes>
-      </Router>
+      <Login /> {/* this will appear in all the registration component*/}
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+      </Routes>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
+import shopper from "../components/assets/asian woman.png";
 
 const Home = () => {
   return (
@@ -10,13 +12,51 @@ const Home = () => {
         {/** Search Container*/}
         <div className="search-cont">
           <div className="search">
-            <input className='input' type="text" placeholder=" Search...“Disposable Cups”" />
-            <button className="search-button" type="button">Search</button>
+            <input
+              className="input"
+              type="text"
+              placeholder=" Search...“Disposable Cups”"
+            />
+            <button className="search-button" type="button">
+              Search
+            </button>
           </div>
         </div>
 
         {/*homepage content*/}
-        div.
+        <div className="landingpage-body">
+          <div className="textwrap">
+            <div className="text1">
+              <p>
+                Sell online with{" "}
+                <span className="idanM">
+                  <b>idanMart</b>
+                </span>
+              </p>
+            </div>
+            <div className="text2">
+              <p>Let over 1,000000 buyers see your Products</p>
+            </div>
+            <div className="text3">
+              <p>
+                Search anything you want by category and get it swiftly in
+                idanMart
+              </p>
+            </div>
+            <div className="register-button">
+              <Link to="/registration">
+                <button type="button">Get Started</button>
+              </Link>
+              <button type="button">offers</button>
+            </div>
+          </div>
+
+          {/** Image in Landing page */}
+          <div className="image">
+            <div className="ellipse2" />
+            <img className="shopper" src={shopper} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
