e997ecc78a075e9cce037fc5f3e10db1ad441096
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-11-17 12:13:32 +0100

    navigating

--- src/components/appnavbar/appnavbar.jsx
+++ src/components/appnavbar/appnavbar.jsx
@@ -8,10 +8,10 @@ export const Appnavbar = () => {
         <section>
             <nav  className='navbox'>
                 <div className='appnav'>
-                    <Link to="/home" className='link'>Home</Link>
+                    <Link to="/About" className='link'>Home</Link>
                     <Link to="/ourservices" className='link'>Our Services</Link>
                     <Link to="/About" className='link'>About Us</Link>
-                    <Link to="/VendorsClique" className='link'>Vendors Clique</Link>
+                    <Link to="/Vendor/dashboard" className='link'>Dashboard</Link>
                     <Link to="/contact" className='link'>Contact us</Link>
                 </div>
             </nav>
