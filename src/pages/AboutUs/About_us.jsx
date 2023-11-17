642082fcfe4e18e142b94b2a835a3e01277e32bd
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-11-17 07:24:45 +0100

    About

--- src/pages/AboutUs/About_us.jsx
+++ src/pages/AboutUs/About_us.jsx
@@ -3,15 +3,6 @@ import "./About_us.css";
 import { Appnavbar } from "../../components/appnavbar/appnavbar";
 import imgs from "../../components/assets/about1.jpeg";
 import imgs2 from "../../components/assets/about2.jpeg";
-import card from "../../components/assets/logos_mastercard.svg";
-import atm from "../../components/assets/noto_credit-card.svg";
-import money from "../../components/assets/Group.svg";
-import visa from "../../components/assets/1495815261-jd08_84586 1.png";
-import interswitch from "../../components/assets/download__10_-removebg-preview 1.png";
-import facebook from '../../components/assets/logos_facebook.svg';
-import linkedin from '../../components/assets/Group1.svg';
-import X from '../../components/assets/simple-icons_x.svg';
-import instagram from '../../components/assets/mdi_instagram.svg';
 import { Footer } from "../../components/Footer/Footer";
 
 
